40bc7d071606a157a171694619b09f4a3914d523
malloryporche <malloryburke@gmail.com>
2023-09-15 23:31:59 -0400

    Pass 2nd test - sum integers

--- src/recursion.js
+++ src/recursion.js
@@ -24,6 +24,17 @@ let factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 let sum = function(array) {
+  // declare result var
+  var result = 0;
+
+  if (!Array.isArray(array)) {
+    return result += array;
+  }
+  array.forEach(function(item) {
+    result += sum(item);
+  });
+  //return result var
+  return result;
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
